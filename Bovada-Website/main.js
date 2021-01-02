6d546874c7a2356caea0e14ee7b0da95362cb3ac
hayubnustseecs <hayub.bese18seecs@seecs.edu.pk>
2021-01-02 17:44:19 +0500

    initial version error free ready

--- Bovada-Website/main.js
+++ Bovada-Website/main.js
@@ -8,17 +8,21 @@ $(document).ready(function () {
 
          var _heading="", _timestamp="", _teams="", _spread="", _win="", _total="";
          var table = document.getElementById("next-events");
-         var tstamp;
-         var tr,td, headDiv,h2,dateTimeDiv, date, time, bets, aTag, team1Div, team2Div,team1Name,team2Name,team1Spread,team2Spread,team1Win,team2Win,team1Total,team2Total;
+         var tbody = document.createElement('tbody');
+         var headtr,headtd,tr,td, headDiv,h2,dateTimeDiv, date, time, bets, aTag, team1Div, team2Div,team1Name,team2Name,team1Spread,team2Spread,team1Win,team2Win,team1Total,team2Total;
          for(var i=0; i<arrItems.length;i++){
+            headtr = document.createElement('tr')
+            headtd = document.createElement('td')
             tr = document.createElement('tr');
             td = document.createElement('td');
-            headDiv = document.createElement('div');
-            headDiv.setAttribute('id','heading-div');
+            
             h2 = document.createElement('h2');
             _heading = arrItems[i].header;
             console.log(_heading)
             h2.innerHTML = _heading;
+            headtd.appendChild(h2);
+            headtr.appendChild(headtd);
+            tbody.appendChild(headtr);
             for(var match in arrItems[i].matches){
                _timestamp = arrItems[i].matches[match].timestamp.split("\n")
                _teams = arrItems[i].matches[match].teams.split("\n")
@@ -27,7 +31,7 @@ $(document).ready(function () {
                _total = arrItems[i].matches[match].total.split("\n")
                // console.log("Timestamp: "+_timestamp+"\nTeams: "+_teams+"\nSpread: "+_spread+"\nWin: "+_win+"\nTotal: "+_total)
 
-               console.log(_timestamp)
+               console.log(_teams)
 
                dateTimeDiv = document.createElement('div');
                dateTimeDiv.setAttribute('id', 'date-time-div');
@@ -41,6 +45,10 @@ $(document).ready(function () {
                   date.innerHTML = _timestamp[0]+" (Date)";
                   time.innerHTML = _timestamp[1]+" (Time)";
                }
+
+               headDiv = document.createElement('div');
+               headDiv.setAttribute('id','heading-div');
+
                aTag = document.createElement('a');
                team1Div = document.createElement('div')
                team2Div = document.createElement('div')
@@ -69,30 +77,35 @@ $(document).ready(function () {
                team2Win.setAttribute('id','team2-win')
                team2Total.setAttribute('id','team2-total')
 
-               if(_teams.length>0){
+               if(typeof(_teams) === 'undefined' || _teams == null || _teams == ""){
+                  team1Name.innerHTML = "Not Specified";
+                  team2Name.innerHTML = "Not Specified";
+                  bets.innerHTML = "+0 Bets";
+               }else{
                   team1Name.innerHTML = _teams[0];
                   team2Name.innerHTML = _teams[1];
                   bets.innerHTML = _teams[2]
                }
-               if(_spread.length>0){
+               if(typeof(_spread) === 'undefined' || _spread == null || _spread == ""){
+                  team1Spread.innerHTML = "None - Spread";
+                  team2Spread.innerHTML = "None - Spread";
+               }else{
                   team1Spread.innerHTML = _spread[0]+" - Spread";
                   team2Spread.innerHTML = _spread[1]+" - Spread";
                }
-               else{
-                  team1Spread.innerHTML = "None - Spread";
-                  team2Spread.innerHTML = "None - Spread"
-               }
-               if(_win.length>0){
+               if(typeof(_win) === 'undefined' || _win == null || _win == ""){
+                  team1Win.innerHTML = "∞ - Win";
+                  team2Win.innerHTML = "∞ - Win";
+               }else{
                   team1Win.innerHTML = _win[0]+" - Win";
                   team2Win.innerHTML = _win[1]+" - Win";
                }
-               if(_total.length>0){
-                  team1Total.innerHTML = _total[0]+" - Total";
-                  team2Total.innerHTML = _total[1]+" - Total";
-               }
-               else{
+               if(typeof(_total) === 'undefined' || _total == null || _total == ""){
                   team1Total.innerHTML = "None - Total";
                   team2Total.innerHTML = "None - Total";
+               }else{
+                  team1Total.innerHTML = _total[0]+" - Total";
+                  team2Total.innerHTML = _total[1]+" - Total";
                }
 
                team1Div.appendChild(team1Name);
@@ -115,12 +128,12 @@ $(document).ready(function () {
                
                aTag.appendChild(team1Div);
                aTag.appendChild(team2Div);
+               aTag.setAttribute("href","https://record.revenuenetwork.com/_gK7De1rYvgo2XUl_PBb-mGNd7ZgqdRLk/1/")
 
                dateTimeDiv.appendChild(date)
                dateTimeDiv.appendChild(time)
                dateTimeDiv.appendChild(bets)
                
-               headDiv.appendChild(h2)
                headDiv.appendChild(dateTimeDiv)
                headDiv.appendChild(aTag)
 
@@ -128,8 +141,9 @@ $(document).ready(function () {
                
             }
             tr.appendChild(td)
-            table.appendChild(tr)
-          }
+            tbody.appendChild(tr)
+         }
+         table.appendChild(tbody);
       }
    });
 });
